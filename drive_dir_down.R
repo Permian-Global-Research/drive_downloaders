47b697128a191fbe8d8db160da8fc39b8823f553
Hugh Graham <hugh.a.graham@gmail.com>
2022-10-05 16:36:40 +0100

    rm updates

--- drive_dir_down.R
+++ drive_dir_down.R
@@ -11,3 +11,9 @@ drive_get("Drive-DL-testing") |>
   as_id() |>
   drive_dir_down()
 
+.url <- "https://drive.google.com/drive/folders/1rRPmBZT0OhU-sFDmvWCKEMiYt7Tqh8aM"
+d <- drive_download_dir(drive.folder=.url)
+
+#install.packages(fs)
+library(fs)
+dir_tree(path = "Drive-DL-testing", recurse = TRUE)
